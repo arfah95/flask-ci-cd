07eb8077364bc21f846b5728b6c46acccdab0b4b
arfah <sikanderarfa95@gmail.com>
2025-07-25 16:52:16 +0500

    Initial commit for Flask portfolio site

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
-# Create a Flask app
 app = Flask(__name__)
 
-# Define a route
 @app.route("/")
-def hello():
-    return "Hello, Flask CI/CD!"
+def home():
+    return render_template("index.html")
 
-# Run the app
 if __name__ == "__main__":
     app.run(debug=True)
+
